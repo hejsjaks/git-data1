bb590afaae85d58d07c5535db6ce1fab1726f4e8
hejsjaks <bmy78000@naver.com>
2025-09-25 10:37:56 +0900

    9월25일강의

--- 09-25-0.c
+++ 09-25-0.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-#define N 5
+#define N 5     // 배열의 요소 개수
 int main()
 {
     int foo[N];
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < N; i++) { // 각 요소의 값을 출력
         printf("foo[%d] : ", i);
         scanf("%d",&foo[i]);
     }
     puts("각 요소의 값");
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < N; i++) { // 각 요소의 값을 출력
         printf("foo[%d] = %d\n", i, foo[i]);
     } 
     return 0;
